8817febafe5b8fdef8b393de2beff173446ef64f
P. Colin Manikoth <pmanikoth@ewu.edu>
2023-06-02 17:35:28 -0700

    Fix layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,33 @@
-// My Scripts
+// Scripting
 
-
-// Data source from AirTable API
-// Note: We're using NoCode API to make connecting to AirTable easier
-const url = "https://v1.nocodeapi.com/pmanikoth/airtable/rsqhQzAAKcMyyPPW?tableName=Roster";
+// Data source
+const url = "https://assets.codepen.io/16425/spring23web3.json";
 
 // Get data
 fetch(url)
   .then( response  => response.json())
   .then( data  => {
-    // check-check: get one image
-    // Note: Webflow returns data in array called `items`
-    console.log(data.records);
-    console.log(data.records[0].fields);
-    console.log(data.records[0].fields.Name);
-    console.log(data.records[0].fields.Emoji);
-    console.log(data.records[0].fields.Color);
-    console.log(data.records[0].fields.Image[0].url);
+    
+    // check-check: is the data good?
+    console.log(data);
+    console.log(data.Name);
+    console.log(data.Image);
 
     // get container for data
-    const gallery = document.querySelector(".gallery");
+    const roster = document.querySelector(".roster");
 
     // loop through data
-    data.records.forEach( student => {
+    data.forEach( student => {
       
       // template
       const template = `
           <figure>
-            <figcaption>${student.fields.Name}</figcaption>
-            <p>${student.fields.Emoji}</p>
-            <img src="${student.fields.Image[0].url}" alt="${student.fields.Name}">
+            <figcaption> ${student.Name} </figcaption>
+            <img src=" ${student.Image} " alt=" ${student.Name} ">
           </figure>
        `;
 
       // insert EACH `student` record into container
-      gallery.insertAdjacentHTML("afterbegin", template);
+      roster.insertAdjacentHTML("afterbegin", template);
     });
   });
